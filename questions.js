8384d296774e7bca36a1559dc59a768bb32dcc64
ThiagoFS-lab <122224320+ThiagoFS-lab@users.noreply.github.com>
2023-02-07 21:16:11 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Qual dos seguintes animais se encontra em extinção?",
+      answers: [
+        { option: "Onça-pintada", correct: true },
+        { option: "Uacari", correct: false },
+        { option: "Canário", correct: false },
+        { option: "Macaco-prego", correct: false },
+      ],
+    },
+    {
+      question: "Assinale a alternativa que não apresenta um problema ambiental",
+      answers: [
+        { option: "Caça de diversos animais silvestres", correct: false },
+        { option: "Infiltração da água da chuva no solo", correct: true },
+        { option: "Retirada da vegetação nativa local", correct: false },
+        { option: "Desflorestamento de matas nativas", correct: false },
+      ],
+    },
+    {
+      question: "Qual o nome do acordo, realizado por meio da Conferência das Nações Unidas sobre Mudanças Climáticas do ano de 2015, que estabeleceu medidas de controle do aquecimento global?",
+      answers: [
+        { option: "Acordo de Montreal", correct: false },
+        { option: "Acordo de Kyoto", correct: false },
+        { option: "Acordo de Paris", correct: true },
+        { option: "Acordo de São Paulo", correct: false },
+      ],
+    },
+    {
+      question: "Assinale a alternativa que apresenta uma característica do espaço ambiental brasileiro",
+      answers: [
+        { option: "Existência de uma pequena quantidade de espécies vegetais", correct: false },
+        { option: "Pequena diversidade geográfica do espaço físico local", correct: false },
+        { option: "Ocorrência de graves impactos ambientais no meio natural", correct: true },
+        { option: "Presença de grandes espaços naturais bastante homogêneos", correct: false },
+      ],
+    },
+    {
+      question: "Das opções abaixo, a que não representa um impacto ambiental é",
+      answers: [
+        { option: "chuva ácida", correct: false },
+        { option: "desertificação", correct: false },
+        { option: "mobilidade urbana", correct: true },
+        { option: "assoreamento dos rios", correct: false },
+      ],
+    },
+    {
+      question: "Sobre as queimadas, é incorreto afirmar",
+      answers: [
+        { option: "uma das consequências das queimadas é a mudança da temperatura e umidade do solo", correct: false },
+        { option: "as queimadas somente acontecem de maneira intencional, ou seja, por pessoas que provocam os incêndios", correct: true },
+        { option: "as queimadas intensificam o efeito estufa e o aquecimento global", correct: false },
+        { option: "diversas práticas agrícolas estão relacionadas com as causas das queimadas", correct: false },
+      ],
+    },
+    {
+      question: "As ilhas de calor representam um dos problemas ambientais urbanos. Esse fenômeno climático acontece devido",
+      answers: [
+        { option: "a elevação das temperaturas em algumas zonas urbanas", correct: true },
+        { option: "a diminuição da densidade demográfica dos centros urbanos", correct: false },
+        { option: "aos microclimas periféricos que afetam diretamente as cidades", correct: false },
+        { option: "ao aumento das queimadas nas zonas rurais", correct: false },
+      ],
+    },
+    {
+      question: "Todas as alternativas abaixo trazem exemplos de ações positivas relacionadas com a consciência ambiental, exceto",
+      answers: [
+        { option: "o uso de automóveis", correct: true },
+        { option: "a economia de água e de energia", correct: false },
+        { option: "o descarte correto do lixo", correct: false},
+        { option: "a redução do consumo", correct: false },
+      ],
+    },
+    {
+      question: "A poluição radioativa é considerada um dos piores tipos de poluição, pois",
+      answers: [
+        { option: "provoca o desmatamento da cobertura vegetal", correct: false },
+        { option: "acelera o processo de desertificação do planeta", correct: false },
+        { option: "libera gases e elementos tóxicos na atmosfera", correct: true },
+        { option: "utiliza elementos artificiais criados em laboratório", correct: false },
+      ],
+    },
+    {
+      question: "O aquecimento global e o efeito estufa são fenômenos que se relacionam. Sobre isso, é correto afirmar",
+      answers: [
+        { option: "O efeito estufa é um fenômeno natural decorrente dos movimentos de rotação e translação da Terra", correct: false },
+        { option: "O aquecimento global é o resultado da intensificação do efeito estufa e consiste no aumento das temperaturas médias do planeta e das águas dos oceanos", correct: true },
+        { option: "Os principais gases do efeito estufa que dificultam a dispersão da radiação solar e provocam maior retenção de calor são o hélio e o radônio", correct: false },
+        { option: "As principais causas do aquecimento global estão relacionadas com a maritimidade e a continentalidade", correct: false },
+      ],
+    },
+  ];
